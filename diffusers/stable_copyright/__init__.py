a9b35b222a81b21fd910a2337319599027078304
CaradryanLiang <caradryan2022@gmail.com>
2024-05-10 14:47:53 -0700

    add dino for drc

--- diffusers/stable_copyright/__init__.py
+++ diffusers/stable_copyright/__init__.py
@@ -6,3 +6,7 @@ from .secmi_scheduling_ddim import SecMIDDIMScheduler
 from .pia_pipeline_stable_diffusion import PIAStableDiffusionPipeline
 
 from .pfami_pipeline_stable_diffusion import PFAMIStableDiffusionPipeline
+
+from .drc_dino_utils import *
+from .drc_dino_vision_transformer import *
+from .drc_pipeline_mask_prediction import *
