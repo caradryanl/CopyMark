a1a7b9134224e1875ca7fcf40a119d090f9ef50b
CaradryanLiang <caradryan2022@gmail.com>
2024-05-22 23:33:39 -0700

    low batchsize

--- diffusers/scripts/exp_ldm_sd_eval.sh
+++ diffusers/scripts/exp_ldm_sd_eval.sh
@@ -1,6 +1,6 @@
-python scripts/train_secmi.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval --eval True --batch-size 32
-python scripts/train_pia.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval  --eval True --batch-size 32
-python scripts/train_pfami.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval --eval True --batch-size 32
-python scripts/train_secmi.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/ --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 24
-python scripts/train_pia.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/  --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 24
-python scripts/train_pfami.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/ --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 24
+python scripts/train_secmi.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval --eval True --batch-size 24
+python scripts/train_pia.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval  --eval True --batch-size 24
+python scripts/train_pfami.py --model-type ldm --ckpt-path ../models/diffusers/ldm-celebahq-256/ --member-dataset celeba-hq-2-5k-eval --holdout-dataset ffhq-2-5k-eval --eval True --batch-size 24
+python scripts/train_secmi.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/ --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 8
+python scripts/train_pia.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/  --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 8
+python scripts/train_pfami.py --model-type sd --ckpt-path ../models/diffusers/stable-diffusion-v1-5/ --member-dataset laion-aesthetic-2-5k-eval --holdout-dataset coco2017-val-2-5k-eval --eval True --batch-size 8
