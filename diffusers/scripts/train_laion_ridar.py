edab3347081b486c79852390f7cbfa56cd60788b
CaradryanLiang <caradryan2022@gmail.com>
2024-06-23 08:38:09 -0700

    add laion ridar

--- diffusers/scripts/train_laion_ridar.py
+++ diffusers/scripts/train_laion_ridar.py
@@ -11,15 +11,54 @@ import json,time
 from accelerate import Accelerator
 import pickle
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import SGDClassifier
 from sklearn import preprocessing
-from xgboost import XGBClassifier
+from xgboost import XGBRegressor
+
+from torchvision import transforms
+from torch.utils.data import DataLoader, ConcatDataset
+from transformers import CLIPTokenizer
+
+from copymark import SecMIDDIMScheduler, GSAStableDiffusionPipeline
+from copymark import benchmark, test, Dataset, collate_fn
 
 
-from copymark import GSALatentDiffusionPipeline, SecMIDDIMScheduler, GSAStableDiffusionPipeline, GSAStableDiffusionXLPipeline
-from copymark import load_dataset, benchmark, test
+def load_dataset(dataset_root, ckpt_path, dataset: str | list[str], batch_size: int=6, model_type='sd'):
+    resolution = 512
+    transform = transforms.Compose(
+        [
+            transforms.Resize(resolution, interpolation=transforms.InterpolationMode.BILINEAR),
+            transforms.CenterCrop(resolution),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
+        ]
+    )
+    tokenizer = CLIPTokenizer.from_pretrained(
+        ckpt_path, subfolder="tokenizer", revision=None
+    )
 
+    if isinstance(dataset, str):
+        train_dataset = Dataset(
+            dataset=dataset,
+            img_root=dataset_root,
+            transforms=transform, 
+            tokenizer=tokenizer)
+    else:
+        train_datasets = []
+        for item in dataset:
+            train_datasets.append(
+                Dataset(
+                    dataset=item,
+                    img_root=dataset_root,
+                    transforms=transform, 
+                    tokenizer=tokenizer
+                )
+            )
+        train_dataset = ConcatDataset(train_datasets)
+
+    train_dataloader = DataLoader(
+        train_dataset, pin_memory=True, shuffle=False, collate_fn=collate_fn, batch_size=batch_size
+    )
+    return train_dataset, train_dataloader
 
 def load_pipeline(ckpt_path, device='cuda:0'):
     pipe = GSAStableDiffusionPipeline.from_pretrained(ckpt_path, torch_dtype=torch.float16)
@@ -49,24 +88,12 @@ def get_reverse_denoise_results(pipe, dataloader, device, demo):
             accelerator=accelerator, optimizer=optimizer, prompt=prompts, latents=latents, \
             prompt_embeds=encoder_hidden_states, guidance_scale=1.0, num_inference_steps=20, gsa_mode=2)
         gsa_features = out.gsa_features # # [bsz x Tensor(num_p)]
-        # print(f"gsa: {gsa_features}")
-
-        # print(f'posterior {posterior_results[0].shape}')
-
         for feature in gsa_features:
             features.append(feature.detach().clone().cpu())
-        
         if demo and batch_idx > 0:
             break
 
     return torch.stack(features, dim=0), path_log
-
-def get_reverse_denoise_results_ddp(pipe, dataloader):
-    '''
-        TODO:
-        Implement the ddp sampling
-    '''
-    return None, None
 
 def preprocess(member, non_member):
     member = member[0:non_member.shape[0]]
@@ -85,49 +112,23 @@ def preprocess(member, non_member):
         has_inf = np.isinf(array).any()
         return has_nan or has_inf
     
-    print(has_nan_inf(x))
+    print("X includes nan or inf? {}".format(has_nan_inf(x)))
     return x, y
 
 def train_xgboost(member_features, nonmember_features):
     x, y = preprocess(member_features, nonmember_features)
-    # xgb = SGDClassifier()
-    model = XGBClassifier(n_estimators=200)
+    model = XGBRegressor(n_estimators=50, max_depth=2)
     model.fit(x, y)
-
-    def get_batches(X, y, batch_size):
-        n_samples = X.shape[0]
-        for start in range(0, n_samples, batch_size):
-            end = min(start + batch_size, n_samples)
-            yield X[start:end], y[start:end]
-    
-    # Training the model incrementally
-    # for X_batch, y_batch in get_batches(x, y, batch_size=100):
-    #     xgb.partial_fit(X_batch, y_batch, classes=np.unique(y))
-
-    # y_pred = batch_predict(xgb, x, batch_size=100)
     y_pred = model.predict(x)
-
     member_scores = torch.tensor(y_pred[y <= 0.5])
     nonmember_scores = torch.tensor(y_pred[y > 0.5])
     return model, member_scores, nonmember_scores
 
-def batch_predict(model, X, batch_size):
-    n_samples = X.shape[0]
-    predictions = []
-    for start in range(0, n_samples, batch_size):
-        end = min(start + batch_size, n_samples)
-        X_batch = X[start:end]
-        batch_predictions = model.predict(X_batch)
-        predictions.extend(batch_predictions)
-    return np.array(predictions)
-
 def test_xgboost(xgb_save_path, member_features, nonmember_features):
     x, y = preprocess(member_features, nonmember_features)
-    
     with open(xgb_save_path, 'rb') as f:
         model = pickle.load(f)
 
-    # y_pred = batch_predict(xgb, x, batch_size=100)
     y_pred = model.predict(x)
     member_scores = torch.tensor(y_pred[y <= 0.5])
     nonmember_scores = torch.tensor(y_pred[y > 0.5])
@@ -143,73 +144,51 @@ def normalize(array):
 
 def main(args):
     start_time = time.time()
-
-    _, holdout_loader = load_dataset(args.dataset_root, args.ckpt_path, args.holdout_dataset, args.batch_size, args.model_type)
-    _, member_loader = load_dataset(args.dataset_root, args.ckpt_path, args.member_dataset, args.batch_size, args.model_type)
-
-    pipe = load_pipeline(args.ckpt_path, args.device, args.model_type)
+    _, holdout_loader = load_dataset(args.dataset_root, args.ckpt_path, args.holdout_dataset, args.batch_size)
+    _, member_loader = load_dataset(args.dataset_root, args.ckpt_path, args.member_dataset, args.batch_size)
+    pipe = load_pipeline(args.ckpt_path, args.device)
 
     if not args.use_ddp:
-
-        if not os.path.exists(args.output):
-            os.mkdir(args.output)
+        os.makedirs(args.output, exist_ok=True)
 
         # phase 1: get the training feature
         member_features, member_path_log = get_reverse_denoise_results(pipe, member_loader, args.device, args.demo)
         nonmember_features, nonmember_path_log = get_reverse_denoise_results(pipe, holdout_loader, args.device, args.demo)
-         
-        # save the features
         member_features, nonmember_features = member_features.numpy(), nonmember_features.numpy()
         features = np.vstack((member_features, nonmember_features))
-
         if not args.eval:
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_features.npy', 'wb') as f:
+            with open(args.output + f'laion_ridar_features.npy', 'wb') as f:
                 np.save(f, features)
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_image_log.json', 'w') as file:
+            with open(args.output + f'laion_ridar_image_log.json', 'w') as file:
                 json.dump(dict(member=member_path_log, nonmember=nonmember_path_log), file, indent=4)
 
             # train a xgboost
             xgb, member_scores, nonmember_scores = train_xgboost(member_features, nonmember_features)
-            with open(args.output + f'xgboost_gsa_{args.gsa_mode}_{args.model_type}.bin', 'wb') as f:
+            with open(args.output + f'xgboost_laion_ridar.bin', 'wb') as f:
                 pickle.dump(xgb, f)
 
-            benchmark(member_scores, nonmember_scores, f'gsa_{args.gsa_mode}_{args.model_type}_score', args.output)
+            benchmark(member_scores, nonmember_scores, f'laion_ridar_score', args.output)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
             running_time = dict(running_time=elapsed_time)
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_running_time.json', 'w') as file:
+            with open(args.output + f'laion_ridar_running_time.json', 'w') as file:
                 json.dump(running_time, file, indent=4)
         else:
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_features_test.npy', 'wb') as f:
+            with open(args.output + f'laion_ridar_features_test.npy', 'wb') as f:
                 np.save(f, features)
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_image_log_test.json', 'w') as file:
+            with open(args.output + f'laion_ridar_image_log_test.json', 'w') as file:
                 json.dump(dict(member=member_path_log, nonmember=nonmember_path_log), file, indent=4)
 
             # test the trained xgboost
-            xgb_save_path = args.threshold_root + f'{args.model_type}/gsa_{args.gsa_mode}/xgboost_gsa_{args.gsa_mode}_{args.model_type}.bin'
+            xgb_save_path = args.threshold_root + f'laion_ridar/xgboost_laion_ridar.bin'
             member_scores, nonmember_scores = test_xgboost(xgb_save_path, member_features, nonmember_features)
 
-            threshold_path = args.threshold_root + f'{args.model_type}/gsa_{args.gsa_mode}/'
-            test(member_scores, nonmember_scores, f'gsa_{args.gsa_mode}_{args.model_type}_score', args.output, threshold_path)
+            threshold_path = args.threshold_root + f'laion_ridar/'
+            test(member_scores, nonmember_scores, f'laion_ridar_score', args.output, threshold_path)
 
-            TP = (member_scores <= 0.5).sum()
-            TN = (nonmember_scores > 0.5).sum()
-            FP = (nonmember_scores <= 0.5).sum()
-            FN = (member_scores > 0.5).sum()
-            TPR = TP / (TP + FN)
-            FPR = FP / (FP + TN)
-
-            extra_output = dict(TPR=TPR.item(), FPR=FPR.item())
-            with open(args.output + f'gsa_{args.gsa_mode}_{args.model_type}_score' + '_extra.json', 'w') as file:
-                json.dump(extra_output, file, indent=4)
-
-            
     else:
         raise NotImplementedError('DDP not implemented')
-    
-    
-    
 
 
 def fix_seed(seed):
@@ -223,18 +202,15 @@ def fix_seed(seed):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--member-dataset', default='laion-aesthetic-2-5k')
-    parser.add_argument('--holdout-dataset', default='coco2017-val-2-5k')
+    parser.add_argument('--member-dataset', default='laion-aesthetic/eval')
+    parser.add_argument('--holdout-dataset', nargs='+', default=['cc12m/eval', 'yfcc100m/eval', 'datacomp/eval', 'coco2017-val/eval'])
     parser.add_argument('--dataset-root', default='datasets/', type=str)
     parser.add_argument('--seed', type=int, default=10)
-    parser.add_argument('--ckpt-path', type=str, default='../models/diffusers/stable-diffusion-v1-5/')
-    # parser.add_argument('--ckpt-path', type=str, default='../models/diffusers/ldm-celebahq-256/')
+    parser.add_argument('--ckpt-path', type=str, default='models/diffusers/stable-diffusion-v1-5/')
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--output', type=str, default='outputs/')
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--use-ddp', type=bool, default=False)
-    parser.add_argument('--gsa-mode', type=int, default=1, choices=[1, 2])
-    parser.add_argument('--model-type', type=str, choices=['sd', 'sdxl', 'ldm', 'kohaku'], default='sd')
     parser.add_argument('--demo', type=bool, default=False)
     parser.add_argument('--eval', type=bool, default=False)
     parser.add_argument('--threshold-root', type=str, default='experiments/')
