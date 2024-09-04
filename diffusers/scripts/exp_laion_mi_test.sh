0600037c30f52a1a57bdcf4f035740fed77e0482
caradryanl <caradryan2022@gmail.com>
2024-09-04 16:01:26 +0000

    correct laion mi

--- diffusers/scripts/exp_laion_mi_test.sh
+++ diffusers/scripts/exp_laion_mi_test.sh
@@ -1,5 +1,5 @@
-python scripts/train_secmi.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/ --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 64 --eval True
-python scripts/train_pia.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/  --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 64 --eval True
-python scripts/train_pfami.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/ --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 64 --eval True
+python scripts/train_secmi.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/ --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 32 --eval True
+python scripts/train_pia.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/  --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 32 --eval True
+python scripts/train_pfami.py --model-type sd --ckpt-path models/stable-diffusion-v1-5/ --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 32 --eval True
 python scripts/train_gsa.py --model-type sd --gsa-mode 1 --ckpt-path models/stable-diffusion-v1-5/ --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 2 --eval True
 python scripts/train_gsa.py --model-type sd --gsa-mode 2 --ckpt-path models/stable-diffusion-v1-5/  --member-dataset laion-mi-member-test --holdout-dataset laion-mi-nonmember-test --batch-size 2 --eval True
